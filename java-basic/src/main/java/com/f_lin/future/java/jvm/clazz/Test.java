60e30ba5b506014ff39eb5a6062b594096b45068
fengjunlin <Pass2word>
2022-04-02 18:36:15 +0800

    一些积累

---
+++ java-basic/src/main/java/com/f_lin/future/java/jvm/clazz/Test.java
@@ -0,0 +1,23 @@
+package com.f_lin.future.java.jvm.clazz;
+
+/**
+ * @author F_lin fengjunlin@sobey.com
+ * @version 1.0
+ * @since 2022/3/18
+ */
+public class Test {
+
+    private int x;
+
+    public void inc() {
+        ++x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
