4ce67ac0c04cca15b822cc8375b8a00c787ba1f1
fengjunlin <fengjunlin@sobey.com>
2023-02-23 19:17:19 +0800

    线程池相关

--- java-basic/src/main/java/com/f_lin/future/java/juc/ThreadLocalDetailed.java
+++ java-basic/src/main/java/com/f_lin/future/java/juc/ThreadLocalDetailed.java
@@ -1,5 +1,9 @@
 package com.f_lin.future.java.juc;
 
+import io.netty.util.concurrent.FastThreadLocal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * @author F_lin fengjunlin@sobey.com
  * @version 1.0
@@ -7,5 +11,7 @@ package com.f_lin.future.java.juc;
  */
 public class ThreadLocalDetailed {
 
+    ThreadLocal<DateFormat> dfThreadLocal = ThreadLocal.withInitial(()-> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    FastThreadLocal<DateFormat> fThreadLocal = new FastThreadLocal<>();
 
 }
